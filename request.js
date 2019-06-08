cf1ff3ab8337b79c90e8482e311e0011981b8e2c
ryan_radder <ryanradder0@gmail.com>
2019-06-08 16:10:42 +0200

    Changed stuff

--- request.js
+++ request.js
@@ -1,7 +1,7 @@
 let contract = artifacts.require("TestConsumer");
 let contract_address = '0x54c845cdb69466e57b096b174ff01c957b02de50';
-let oracle_address = '0x7a5b6329cb2f68ede093faf8c743508fa01d3535';
-let job_id = '7a97dc29650a4c1cb4e4624367ef3a6d';
+let oracle_address = '0x84a27fa96e01c93c448bb66752e7327745175722';
+let job_id = '92c81e39b45e4700b094eea18f51e793';
 
 module.exports = function() {
 
@@ -10,7 +10,9 @@ module.exports = function() {
     let ins = await contract.at(contract_address);
     console.log('Calling requestEthereumPrice');
     let res = await ins.requestEthereumPrice(oracle_address, job_id);
+    console.log('Request made..\n\n');
     console.log(JSON.stringify(res));
+    console.log('');
   }
   getEhereumPrice();
 };
