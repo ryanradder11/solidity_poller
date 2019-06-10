746dd30ad11e945b9c9e6d399cb0427b14b8a968
ryan_radder <ryanradder0@gmail.com>
2019-06-10 13:03:39 +0200

    Changed to binary should not be retrieved constantly, also setup for oracle contract integration

--- request.js
+++ request.js
@@ -1,20 +1,30 @@
-let contract = artifacts.require("TestConsumer");
+let consumerContract = artifacts.require("TestConsumer");
+let oracleContract = artifacts.require("TestOracle");
 let contract_address = '0x54c845cdb69466e57b096b174ff01c957b02de50';
 let oracle_address = '0x84a27fa96e01c93c448bb66752e7327745175722';
 let job_id = '92c81e39b45e4700b094eea18f51e793';
 
+let consummerInstance = null;
+let oracleInstance = null;
+
 module.exports = function() {
 
   async function getEhereumPrice() {
-    console.log('Retrieving deployed contract binary...');
-    let ins = await contract.at(contract_address);
+    if(consummerInstance === null){
+      console.log('Retrieving deployed contract binary...');
+       consummerInstance = await consumerContract.at(contract_address);
+    }
     console.log('Calling requestEthereumPrice');
-    let res = await ins.requestEthereumPrice(oracle_address, job_id);
+    let res = await consummerInstance.requestEthereumPrice(oracle_address, job_id);
     console.log('Request made..\n\n');
-    console.log(JSON.stringify(res));
-    console.log('');
+    console.log(JSON.stringify(res) + '\n\n');
+
+    // if(oracleInstance === null){
+    //   console.log('Retrieving oracle instance');
+    //   oracleInstance = await oracleContract.at(oracle_address);
+    // }
+
   }
-  // getEhereumPrice();
   setInterval(() =>{
     getEhereumPrice();
   }, 1000 * 60 * 3);
